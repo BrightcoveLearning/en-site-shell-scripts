15cf1a4a0a05c0444503a2e4ba3d2c664dcfd778
Robert Crooks <rcrooks@brightcove.com>
2021-08-24 17:49:43 -0400

    refactored scripts

--- bundle-install.sh
+++ bundle-install.sh
@@ -1,109 +1,11 @@
-cd ../general-en
-git pull
+sudo chmod -R g+w /Users/rcrooks/github
+git config pull.rebase false
+git config --global pull.rebase false
+sitesArray=(apis-en audience-en beacon-en beacon-support-en beacon-private-en campaign-en docs-support-home-en engage-en gallery-en general-en integrations-en live-en player-en sdks-en social-en ssai-enzencoder-en)
+for ((i = 1; i <= $#sitesArray; i++))
+do 
+  cd "../$sitesArray[i]"
+  git pull
 bundle install
-echo 'bundle installed general content'
-# apis
-cd ../apis-en
-git pull
-bundle install
-echo 'bundle installed apis'
-# audience
-cd ../audience-en
-git pull
-bundle install
-echo 'bundle installed audience'
-# beacon
-cd ../beacon-
-git pull
-bundle install
-echo 'bundle installed beacon'
-# beacon support
-cd ../beacon-support-en
-git pull
-bundle install
-echo 'bundle installed beacon support'
-# beacon private
-cd ../beacon-private-en
-git pull
-bundle install
-echo 'bundle installed beacon private'
-# campaign
-git pull
-cd ../campaign-en
-bundle install
-echo 'bundle installed campaign'
-# docs-integrations
-cd ../integrations-en
-git pull
-bundle install
-echo 'bundle installed docs-integrations'
-# docs-support-home
-cd ../docs-support-home-en
-git pull
-bundle install
-echo 'bundle installed docs-support-home'
-# engage
-cd ../engage-en
-git pull
-bundle install
-echo 'bundle installed engage'
-# gallery
-cd ../gallery-en
-git pull
-bundle install
-echo 'bundle installed gallery'
-# jekyll-template
-cd ../jekyll-template
-git pull
-bundle install
-echo 'bundle installed jekyll-template'
-# live
-cd ../live-en
-git pull
-bundle install
-echo 'bundle installed live'
-# player-dev
-cd ../player-en
-git pull
-bundle install
-echo 'bundle installed player-dev'
-# sdks
-cd ../sdks-en
-git pull
-bundle install
-echo 'bundle installed sdks'
-# social
-cd ../social-en
-git pull
-bundle install
-echo 'bundle installed social'
-# ssai
-cd ../ssai-en
-git pull
-bundle install
-echo 'bundle installed ssai'
-# studio
-cd ../studio-en
-git pull
-bundle install
-echo 'bundle installed studio'
-# zencoder-support
-cd ../zencoder-en
-git pull
-bundle install
-echo 'bundle installed zencoder-support'
-# test site
-cd ../test-site
-git pull
-bundle install
-echo 'bundle installed test-site'
-# stagine site
-cd ../beacon-la
-git pull
-bundle install
-echo 'bundle installed staging-site'
-cd ../beacon-help
-git pull
-bundle install
-echo 'bundle installed beacon-help'
-echo 'all finished!'
+  echo 'pulled and bundle installed '$sitesArray[i]
+done
