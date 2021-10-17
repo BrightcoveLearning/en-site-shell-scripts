7e73bcbb4e4100cf9c915cdbefd046779ccd3a16
Robert <rcrooks@brightcove.com>
2021-10-17 13:47:53 -0400

    update scripts

--- clone-all.sh
+++ clone-all.sh
@@ -4,8 +4,6 @@ git config --global pull.rebase false
 sitesArray=(apis-en audience-en beacon-en beacon-support-en beacon-private-en campaign-en docs-support-home-en engage-en gallery-en general-en integrations-en live-en player-en sdks-en social-en ssai-en zencoder-en mstudio-en)
 for ((i = 1; i <= $#sitesArray; i++))
 do 
-  cd "../$sitesArray[i]"
-  git pull
-  bundle update
-  echo 'pulled and bundle updated '$sitesArray[i]
+  git clone 'https://github.com/BrightcoveLearning/'$sitesArray[i]'.git'
+  echo 'cloned '$sitesArray[i]
 done
