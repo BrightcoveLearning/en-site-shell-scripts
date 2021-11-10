4bc0f6c4241dcd9341b8fd6327813789e0a7d775
Robert <rcrooks@brightcove.com>
2021-11-10 13:33:03 -0500

    script fix

--- clone-all.sh
+++ clone-all.sh
@@ -2,6 +2,7 @@ sudo chmod -R g+w /Users/rcrooks/github
 git config pull.rebase false
 git config --global pull.rebase false
 sitesArray=(apis-en audience-en beacon-en beacon-support-en beacon-private-en campaign-en docs-support-home-en engage-en gallery-en general-en integrations-en interactivity-en live-en mstudio-en player-en sdks-en social-en ssai-en zencoder-en)
+cd ../
 for ((i = 1; i <= $#sitesArray; i++))
 do 
   git clone 'https://github.com/BrightcoveLearning/'$sitesArray[i]'.git'
