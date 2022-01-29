31e5a21a00f0c7e7024590d9460701956e7171c8
Robert Crooks <rcrooks@brightcove.com>
2022-01-29 12:26:30 -0500

    add corptv site

--- sitesarray.sh
+++ sitesarray.sh
@@ -1,5 +1,5 @@
 git config --global pull.rebase false
-sitesArray=(apis-en audience-en beacon-en beacon-support-en beacon-private-en campaign-en docs-support-home-en engage-en gallery-en general-en integrations-en interactivity-en live-en mstudio-en player-en sdks-en social-en ssai-en studio-en zencoder-en)
+sitesArray=(apis-en audience-en beacon-en beacon-support-en beacon-private-en campaign-en corptv-en docs-support-home-en engage-en gallery-en general-en integrations-en interactivity-en live-en mstudio-en player-en sdks-en social-en ssai-en studio-en zencoder-en)
 for ((i = 1; i <= $#sitesArray; i++))
 do 
   cd "../$sitesArray[i]"
