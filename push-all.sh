3f5c470a91c39f79875d9cddb975cd6beafa118f
Robert <rcrooks@brightcove.com>
2023-02-10 16:54:50 -0500

    remove defunct sites from arrays

--- push-all.sh
+++ push-all.sh
@@ -1,7 +1,8 @@
 # sudo chmod -R g+w /Users/rcrooks/github
 git config pull.rebase false
 git config --global pull.rebase false
-sitesArray=(apis-en audience-en audience-insights-en beacon-en beacon-support-en beacon-private-en campaign-en docs-support-home-en gallery-en general-en integrations-en interactivity-en live-en mstudio-en player-en sdks-en social-en ssai-en studio-en zencoder-en)
+#  audience-en social-en
+sitesArray=(apis-en audience-insights-en beacon-en beacon-support-en beacon-private-en campaign-en docs-support-home-en gallery-en general-en integrations-en interactivity-en live-en mstudio-en player-en sdks-en ssai-en studio-en zencoder-en)
 for ((i = 1; i <= $#sitesArray; i++))
 do 
   cd "../$sitesArray[i]"
