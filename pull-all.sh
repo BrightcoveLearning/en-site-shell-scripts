6e4a914d5568dbb56bec33318f6f1ca476fcc735
Robert <rcrooks@brightcove.com>
2023-02-24 18:11:46 -0500

    add corptv

--- pull-all.sh
+++ pull-all.sh
@@ -1,7 +1,7 @@
 git config pull.rebase false
 git config --global pull.rebase false
 #  audience-en social-en
-sitesArray=(apis-en audience-insights-en beacon-en beacon-support-en beacon-private-en campaign-en docs-support-home-en gallery-en general-en integrations-en interactivity-en live-en mstudio-en player-en sdks-en ssai-en studio-en zencoder-en)
+sitesArray=(apis-en audience-insights-en beacon-en beacon-support-en beacon-private-en campaign-en corptv-en docs-support-home-en gallery-en general-en integrations-en interactivity-en live-en mstudio-en player-en sdks-en ssai-en studio-en zencoder-en)
 for ((i = 1; i <= $#sitesArray; i++))
 do 
   cd "../$sitesArray[i]"
