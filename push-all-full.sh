265326a604e3bea9bd39ab4c6466abaa0d2a1ca4
Robert <rcrooks@brightcove.com>
2023-07-12 13:12:53 -0400

    removing old sites

--- push-all-full.sh
+++ push-all-full.sh
@@ -2,7 +2,7 @@
 git config pull.rebase false
 git config --global pull.rebase false
 # 
-sitesArray=(apis-en audience-insights-en beacon-en beacon-support-en campaign-en corptv-en docs-support-home-en gallery-en general-en integrations-en interactivity-en live-en mstudio-en player-en sdks-en ssai-en studio-en zencoder-en)
+sitesArray=(apis-en audience-insights-en beacon-en beacon-support-en corptv-en docs-support-home-en gallery-en general-en integrations-en interactivity-en live-en mstudio-en player-en sdks-en ssai-en studio-en zencoder-en)
 for ((i = 1; i <= $#sitesArray; i++))
 do 
   cd "../$sitesArray[i]"
